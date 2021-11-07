1af135755729a9d975e671b5136eb543cc7a7682
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-07 10:58:15 +0530

    Add files via upload

---
+++ additionofNumbers.m
@@ -0,0 +1,6 @@
+%{Adding two numbers%}
+a = input('Please enter the value of a\n');
+b = input('Please enter the value of b\n');
+c = a+b;
+result = sprintf('The addition of two numbers %d + %d = %d:\n',a,b,c);
+disp(result);
